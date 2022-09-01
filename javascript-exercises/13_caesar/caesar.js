987ade07be3e1b9ae27f3533033ca7c44d645306
monk3yd <jegajardog@gmail.com>
2022-09-01 03:21:09 -0300

    Delete because of reloading JS exercises

--- javascript-exercises/13_caesar/caesar.js
+++
@@ -1,6 +0,0 @@
-const caesar = function() {
-
-};
-
-// Do not edit below this line
-module.exports = caesar;
--- javascript-exercises/pigLatin/pigLatin.js
+++
@@ -1,6 +0,0 @@
-function pigLatin(string) {
-
-};
-  
-// Do not edit below this line
-module.exports = pigLatin;
