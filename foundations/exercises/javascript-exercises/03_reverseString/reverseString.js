37be9935c274d28e71a65935c3aaca9090c749b3
monk3yd <jegajardog@gmail.com>
2022-09-11 19:43:43 -0300

    Init + reverse single word exercise 03

--- foundations/exercises/javascript-exercises/03_reverseString/reverseString.js
+++ foundations/exercises/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    i = 0;
+    reverse = [];
+    while (string.length > i) {
+        reverse.unshift(string[i]);
+        i++;
+    }
+    return reverse.join("")
 };
 
 // Do not edit below this line
