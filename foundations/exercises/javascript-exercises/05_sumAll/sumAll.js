b124722e3f49e66ce274c5c09eb5f5fd19e354ea
monk3yd <jegajardog@gmail.com>
2022-09-11 20:45:28 -0300

    Work with large numbers and number range

--- foundations/exercises/javascript-exercises/05_sumAll/sumAll.js
+++ foundations/exercises/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // Init counter
+    let counter = 0; 
 
+    // Iterate through min - max range
+    for (let i = min; i < max + 1; i++) {
+        counter += i;
+    }
+    return counter;
 };
 
 // Do not edit below this line
