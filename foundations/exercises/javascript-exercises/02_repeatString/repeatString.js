ee300fa16310f8af2a5bd64b8446af44530456a9
monk3yd <jegajardog@gmail.com>
2022-09-11 19:37:58 -0300

    Finish JS exercises 02

--- foundations/exercises/javascript-exercises/02_repeatString/repeatString.js
+++ foundations/exercises/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    strings = [];
+    for (let i = 0; i < num; i++) {
+        strings.push(str);
+    }
+    return strings.join("")
 };
 
 // Do not edit below this line
