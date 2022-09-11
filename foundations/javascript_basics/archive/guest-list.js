4d82cce7b60941da4f1e7bbeb20694049d606d6d
monk3yd <jegajardog@gmail.com>
2022-09-11 15:59:50 -0300

    Finish and tweak old exercise

--- foundations/javascript_basics/archive/guest-list.js
+++ foundations/javascript_basics/archive/guest-list.js
@@ -1,8 +1,9 @@
-https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code
 const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
 
 const admitted = document.querySelector('.admitted');
 const refused = document.querySelector('.refused');
+
 admitted.textContent = 'Admit: ';
 refused.textContent = 'Refuse: ';
 
@@ -14,5 +15,6 @@ for (let i = 0; i < people.length; i++) {
         admitted.textContent += `${people[i]}, `;
     }
 }
+
 admitted.textContent = admitted.textContent.slice(0, -2) + '.';
 refused.textContent = refused.textContent.slice(0, -2) + '.';
