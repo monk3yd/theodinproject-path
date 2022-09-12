5a79a6047d3db917a4da8e1171b2b187da7925f1
monk3yd <jegajardog@gmail.com>
2022-09-12 01:05:11 -0300

    Refactor 3 ways to use events without func name

--- foundations/javascript_basics/events.js
+++ foundations/javascript_basics/events.js
@@ -1,10 +1,11 @@
 // onEvent property on DOM object in JS
-// issue: DOM element can only have 1 onclick property
+// Method 2 issue: DOM element can only have 1 onclick property
 const btn = document.querySelector('#btn');
 btn.onclick = () => alert("Hello World");
 
 
 // Attach event listeners to the nodes in JS
+// Method 3: is much more flexible and powerful, though it is a bit more complex to set up
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
     alert("Hello World!");
