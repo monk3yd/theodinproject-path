a209f4f8f97d893c5173c9ca17bafec325250256
monk3yd <jegajardog@gmail.com>
2022-09-11 23:03:39 -0300

    Finish tempConversion exercise 07

--- foundations/exercises/javascript-exercises/07_tempConversion/tempConversion.js
+++ foundations/exercises/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+    let celsius = (fahr - 32) * 5 / 9;
+    return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+    let fahr = (celsius * 9 / 5) + 32;
+    return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
