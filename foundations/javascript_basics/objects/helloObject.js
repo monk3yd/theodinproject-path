a4c0ac40ffed4bc2213bd99af21a005e33afe279
monk3yd <jegajardog@gmail.com>
2022-09-13 15:53:08 -0300

    Finish helloObject exercise

---
+++ foundations/javascript_basics/objects/helloObject.js
@@ -0,0 +1,12 @@
+// https://javascript.info/object
+// Create empty user object
+let user = {};
+
+// Add property name with value John
+user["name"] = "John";
+// Add property surname with value Smith
+user["surname"] = "Smith";
+// Change the value of name to Pete
+user["name"] = "Pete";
+// Remove property name from object
+delete user["name"];
