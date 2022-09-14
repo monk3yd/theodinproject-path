fad629ee5aacd6de74ccd322068d3136b12db4b6
monk3yd <jegajardog@gmail.com>
2022-09-14 03:39:58 -0300

    Finish fibonacci exercise

--- foundations/exercises/javascript-exercises/10_fibonacci/fibonacci.js
+++ foundations/exercises/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n)
 
+    let fibSequence = [1, 1];
+
+    if (n === 1 || n === 2) {
+        return fibSequence[n - 1];
+    } else if (n < 1) {
+        return "OOPS";
+    }
+
+    for (let i = 2; i < n; i++) {
+        let lastNum = fibSequence[i - 1];
+        let beforeLastNum = fibSequence[i - 2];
+        fibSequence.push(lastNum + beforeLastNum);
+    }
+    // console.log(fibSequence);
+    return fibSequence[n - 1];
 };
 
 // Do not edit below this line
