a3240bbf034dbd116c8ab80965c1488b316e472f
monk3yd <jegajardog@gmail.com>
2022-09-14 04:17:37 -0300

    Pass second test

--- foundations/exercises/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ foundations/exercises/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        const currentYear = (new Date()).getFullYear();
 
+        if (!a.yearOfDeath) {
+            lastGuy = currentYear - a.yearOfBirth; 
+            nextGuy = b.yearOfDeath - b.yearOfBirth;
+        } else if (!b.yearOfDeath) {
+            lastGuy = a.yearOfDeath - a.yearOfBirth; 
+            nextGuy = currentYear - b.yearOfBirth;
+        } else {
+            lastGuy = a.yearOfDeath - a.yearOfBirth; 
+            nextGuy = b.yearOfDeath - b.yearOfBirth;
+        }
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
