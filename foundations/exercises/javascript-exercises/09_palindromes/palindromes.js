4bdd4865faef495f6987f3c43615520bcc8863c5
monk3yd <jegajardog@gmail.com>
2022-09-14 02:59:17 -0300

    Finish palindrome exercise

--- foundations/exercises/javascript-exercises/09_palindromes/palindromes.js
+++ foundations/exercises/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // strip string from all symbols, spaces and punctuation
+    let filteredString = string.replace(/!|\.|\s|,|/g, "").toLowerCase();
 
+    // reverse string
+    let reverseString = [...filteredString].reverse().join('').toLowerCase();
+
+    // compare original string to reverse string
+    if (filteredString === reverseString) return true;
+    return false;
 };
 
 // Do not edit below this line
